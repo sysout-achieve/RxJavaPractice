cd8843c64dd01c8a18ab1c13a2ec482a0a5e4ddb
Geon <0408gun@gmail.com>
2020-08-14 18:23:07 +0900

    RxJava CHAPTER.RxAndroid async

--- app/src/main/java/com/gun/rxjavapractice/AsyncTaskActivity.java
+++ app/src/main/java/com/gun/rxjavapractice/AsyncTaskActivity.java
@@ -6,21 +6,66 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.MaybeObserver;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+
 public class AsyncTaskActivity extends AppCompatActivity {
 
     MyAsyncTask myAsyncTask;
     TextView mAndroidTxtView;
+    TextView mRxTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async_task);
         mAndroidTxtView = findViewById(R.id.mAndroidTxtView);
+        mRxTextView = findViewById(R.id.mRxTextView);
         initAndroidAsync();
+        initRxAsync();
     }
 
     private void initAndroidAsync(){
         myAsyncTask = new MyAsyncTask();
         myAsyncTask.execute("Hello","async", "world");
+    }
+
+    private void initRxAsync(){
+        Observable.just("Hello", "rx", "world")
+                .reduce((x,y)-> x+ " "+ y)
+                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(getObserver())
+                .subscribe(
+                        mRxTextView::setText,
+                        Log::d,
+                        ()->Log.d("TAG","DONE")
+                );
+    }
+
+    private MaybeObserver<String> getObserver(){
+        return new MaybeObserver<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onSuccess(@NonNull String s) {
+              mRxTextView.setText(s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e("TAG", e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("TAG", "done");
+            }
+        };
     }
 
     public class MyAsyncTask extends AsyncTask<String, Void, String>{
